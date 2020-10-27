3709feeb8131b930ae0a4ba875e72c709342a488
Shawn H Wang <57317323+Shawn-H-Wang@users.noreply.github.com>
2020-10-27 10:43:25 +0800

    Update myshell.c
    https://blog.csdn.net/selous/article/details/70408688?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param

--- myshell.c
+++ myshell.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+
+#define MAX_COMMOND 1024
+
+// struct commond {
+//   char *cmd;  // commond
+//   char *arg1; // argument1
+//   char *arg2; // argument2
+// };
+
+void execwww(char *cmd)
+{
+  char *agrv[MAX_COMMOND]; // The arguments of the commond string
+  char *buf[MAX_COMMOND];  // The commond of the commond string
+  
+  /*Modify the commond string*/
+  
+}
+
+void modifyCMDSTR()
+{
+}
 
 int main()
 {
-
+  char cmd[MAX_COMMOND];
+  int n;
+  while(1) {
+    printf("www@mtshell $ >");
+    fflush(stdout);
+    
+    n = read(0, cmd, MAX_COMMON);
+    if (n < 0) {
+      printf("READ ERROR");
+    }
+    execwww(cmd);
+  }
+  return 0;
 }
